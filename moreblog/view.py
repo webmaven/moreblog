8a24f03ffee1ef9996cd19519fe43129f73538c9
Michael R. Bernstein <C_Bernstein.Michael@Cerner.com>
2014-12-08 14:27:25 -0600

    Got Bower resources working

--- moreblog/view.py
+++ moreblog/view.py
@@ -1,10 +1,33 @@
+import os
+
 from .main import App, redirect
 from .model import Root, Collection, Post
+import bowerstatic
 
+bower = bowerstatic.Bower()
+
+
+components = bower.components(
+    'app', os.path.join(os.path.dirname(__file__), 'bower_components'))
+
+
+#local = bower.local_components('local', components)
+
+
+#local.component(os.path.join(os.path.dirname(__file__), 'jquery'),
+                #version=None)
+
+
+@App.static_components()
+def get_static_components():
+    #return local
+    return components
 
 @App.chameleon(model=Root)
 def show_posts(self, request):
-     return {
+    request.include('jquery')
+    request.include('bootstrap')
+    return {
          'template': 'base.pt'
      }
 
@@ -21,5 +44,5 @@ def posts_default(self,request):
 def collection_add_submit(self, request):
     title = request.POST.get('title')
     content = request.POST.get('content')
-    post = self.add(title=title, content=content)
+    self.add(title=title, content=content)
     return redirect('/')
